98da19823de0d61b08a0f42025ea77c7242562a8
ElectricTortoise <yaphengyi.yhy@gmail.com>
2025-03-30 23:56:14 +0800

    Added Piece Square Tables
    bench 23525242

--- Core/Evaluation/Evaluation.cs
+++ Core/Evaluation/Evaluation.cs
@@ -12,9 +12,19 @@ namespace Tortoise.Core
     {
         public static int Evaluate(Board board)
         {
-            int materialValue = 0;
+            int positionValue = 0;
             int colourToMove = board.boardState.GetColourToMove();
             int opponentColour = board.boardState.GetOpponentColour();
+            ulong allPieceBitboard = board.allPieceBitboard;
+            int piecesLeft = 0;
+
+            while (allPieceBitboard != 0)
+            {
+                allPieceBitboard &= allPieceBitboard - 1;
+                piecesLeft++;
+            }
+
+            bool endgame = ((piecesLeft <= 10) && ((board.pieceBitboard[PieceType.Queen] & board.colourBitboard[opponentColour]) == 0UL));
 
             for (int pieceType = 0; pieceType < 5; pieceType++) 
             {
@@ -23,18 +33,20 @@ namespace Tortoise.Core
 
                 while (myPieceBitboard != 0)
                 {
-                    materialValue += GetPieceValue(pieceType);
+                    int square = BitOperations.TrailingZeroCount(myPieceBitboard);
+                    positionValue += GetPieceValue(pieceType) + GetPositionValue(pieceType, square, endgame);
                     myPieceBitboard &= myPieceBitboard - 1;
                 }
 
                 while (opponentPieceBitboard != 0)
                 {
-                    materialValue -= GetPieceValue(pieceType);
+                    int square = BitOperations.TrailingZeroCount(opponentPieceBitboard) ^ 0b111000;
+                    positionValue -= GetPieceValue(pieceType) + GetPositionValue(pieceType, square, endgame); ;
                     opponentPieceBitboard &= opponentPieceBitboard - 1;
                 }
             }
 
-            return materialValue;
+            return positionValue;
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -49,6 +61,35 @@ namespace Tortoise.Core
                 PieceType.Queen => SearchOptions.QueenValue,
                 _ => 0
             };
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int GetPositionValue(int pieceType, int square, bool endgame)
+        {
+            if (endgame)
+            {
+                return pieceType switch
+                {
+                    PieceType.Pawn => PieceSquareTable.endgamePawnTable[square],
+                    PieceType.Knight => PieceSquareTable.endgameKnightTable[square],
+                    PieceType.Bishop => PieceSquareTable.endgameBishopTable[square],
+                    PieceType.Rook => PieceSquareTable.endgameRookTable[square],
+                    PieceType.Queen => PieceSquareTable.endgameQueenTable[square],
+                    _ => 0
+                };
+            }
+            else
+            {
+                return pieceType switch
+                {
+                    PieceType.Pawn => PieceSquareTable.midgamePawnTable[square],
+                    PieceType.Knight => PieceSquareTable.midgameKnightTable[square],
+                    PieceType.Bishop => PieceSquareTable.midgameBishopTable[square],
+                    PieceType.Rook => PieceSquareTable.midgameRookTable[square],
+                    PieceType.Queen => PieceSquareTable.midgameQueenTable[square],
+                    _ => 0
+                };
+            }
         }
 
     }
