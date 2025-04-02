dadd42bdac305699069b6939a2ea99bf0d1b795e
ElectricTortoise <yaphengyi.yhy@gmail.com>
2025-04-02 19:41:45 +0800

    Reworked move ordering
    bench 4907328

---
+++ Core/Search/MoveOrderer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tortoise.Core
+{
+    public static unsafe class MoveOrderer
+    {
+        public static void OrderMoves(ref Board board, ref MoveList moveList)
+        {
+            Span<ScoredMove> ScoredMoves = stackalloc ScoredMove[moveList.Length];
+
+            for (int i = 0; i < moveList.Length; i++)
+            {
+                ScoredMove scoredMove = new ScoredMove(ref board, moveList.Moves[i]);
+                ScoredMoves[i] = scoredMove;
+            }
+
+            ScoredMoves.Sort(new MoveSorter());
+
+            for (int i = 0; i < moveList.Length; i++)
+            {
+                moveList.Moves[i] = ScoredMoves[i].move.EncodeMove();
+            }
+        }
+    }
+
+    public class MoveSorter : IComparer<ScoredMove>
+    {
+        public int Compare(ScoredMove move1, ScoredMove move2)
+        {
+            if (move1.score > move2.score) { return -1; }
+            else if (move1.score < move2.score) { return 1; }
+            else { return 0; }
+        }
+    }
+}
---
+++ Core/Search/ScoredMove.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Formats.Asn1.AsnWriter;
+
+namespace Tortoise.Core
+{
+    public unsafe struct ScoredMove
+    {
+        public int score;
+        public Move move;
+
+        public ScoredMove(ref Board board, ref Move move)
+        {
+            this.score = 0;
+            this.move = move;
+
+            if ((move.flag | MoveFlag.PromoteToKnight) != 0)
+            {
+                this.score += 2;
+            }
+
+            if ((move.flag | MoveFlag.Capture) != 0)
+            {
+                this.score += board.pieceTypesBitboard[move.FinalSquare] - board.pieceTypesBitboard[move.StartSquare];
+            }
+        }
+
+        public ScoredMove(ref Board board, ushort move)
+        {
+            int score = 0;
+            this.move = new Move(move);
+
+            MoveFlag flag = (MoveFlag)(move >> 12);
+            int startSquare = (byte)(move >> 6 & 0b111111);
+            int finalSquare = (byte)(move & 0b111111);
+
+            if ((flag & MoveFlag.PromoteToKnight) != 0)
+            {
+                score += 2;
+            }
+
+            if ((flag & MoveFlag.Capture) != 0)
+            {
+                score += board.pieceTypesBitboard[finalSquare] - board.pieceTypesBitboard[startSquare];
+            }
+        }
+    }
+}
